00a631fbfaa66cbd57038f9b7a50b4117a2562af
JunHyung-Lee <ejhhje@naver.com>
2024-08-12 12:11:26 +0900

    [코드정리] Plyer 폴더 정리
    문제 발생 시 복구를 위한 폴더별 저장 - Player 폴더

--- Source/Explosion/Player/EPPlayerController.cpp
+++ Source/Explosion/Player/EPPlayerController.cpp
@@ -83,9 +83,11 @@ void AEPPlayerController::PostInitializeComponents()
 void AEPPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// 입력 컨텍스트 등록
 	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
 	if (Subsystem != nullptr) {
-		Subsystem->AddMappingContext(InputMappingContext, 0);
+		Subsystem->AddMappingContext(InputMappingContext, 0); // 캐릭터는 Priority가 1이다
 	}
 
 	if (IsLocalController())
@@ -118,16 +120,16 @@ void AEPPlayerController::BeginPlay()
 			StartMainGameWidget->SetVisibility(ESlateVisibility::Visible);
 		}
 
-		// 클라이언트인 경우에만 등록되는 대리자들
+		// UI와 입력을 관리하기에 클라이언트인 경우에만 등록되는 대리자들
 		OnStartMainGame.AddUObject(this, &AEPPlayerController::StartMainGame);
 		OnSetupEndMatch.AddUObject(this, &AEPPlayerController::SetupEndMatch);
-
-		ConsoleCommand(TEXT("stat fps"), true);
 	}
 
-	//EnableInput(this);
+	// 매치가 시작하기 전 까지는 입력 비활성화
 	DisableInput(this);
 	SetInputMode(FInputModeGameOnly());
+
+	StartMainGameWidgetHideDelay = 1.0f;
 }
 
 void AEPPlayerController::StartMainGame()
@@ -136,19 +138,18 @@ void AEPPlayerController::StartMainGame()
 	bIsStartMainGame = true;
 	UpdateScoreBoard();
 
-	// 움직임 종료 해제
 	EnableInput(this);
 
-	// 시작 UI 출력 및 숨기기
 	StartMainGameWidget->SetText(FText::FromString(TEXT("START!!!")));
 	StartMainGameWidget->SetTextColorAndOpacity(FLinearColor::Red);
 	
+	// 준비->시작으로 UI를 변경한 이후 1초 뒤에 숨기기
 	FTimerHandle StartMainGameWidgetTimerHandle;
 	GetWorld()->GetTimerManager().SetTimer(StartMainGameWidgetTimerHandle, FTimerDelegate::CreateLambda([&]()
 		{
 			StartMainGameWidget->SetVisibility(ESlateVisibility::Collapsed);
 		}
-	), 1.0f, false);
+	), StartMainGameWidgetHideDelay, false);
 }
 
 void AEPPlayerController::SetupInputComponent()
@@ -159,41 +160,14 @@ void AEPPlayerController::SetupInputComponent()
 		Input->BindAction(ShowingScoreBoard, ETriggerEvent::Started, this, &AEPPlayerController::ShowScoreBoard);
 		Input->BindAction(ShowingScoreBoard, ETriggerEvent::Completed, this, &AEPPlayerController::HideScoreBoard);
 		Input->BindAction(ShowingGameMenu, ETriggerEvent::Started, this, &AEPPlayerController::ShowGameMenu);
-
 	}
 }
 
-//void AEPPlayerController::ClientRPCUpdateScoreBoard_Implementation()
 void AEPPlayerController::UpdateScoreBoard()
 {
 	if (IsLocalController() && ScoreBoardWidget && bIsStartMainGame)
 	{
-		bool bIsScoreBoardUpdated = false;
-		bool bIsKillCountUpdated = false;
-		bool bIsDeathCountUpdated = false;
-
-		//AEPGameState* EPGameState = GetWorld()->GetGameState<AEPGameState>();
-		//if (EPGameState)
-		//{
-		//	bIsScoreBoardUpdated = EPGameState->GetIsScoreBoardReplicated();
-		//}
-
-		//AEPPlayerState* EPPlayerState = GetPlayerState<AEPPlayerState>();
-		//if (EPPlayerState)
-		//{
-		//	bIsKillCountUpdated = EPPlayerState->GetIsKillCountReplicated();
-		//	bIsDeathCountUpdated = EPPlayerState->GetIsDeathCountReplicated();
-		//}
-
-		if (true || (bIsScoreBoardUpdated && bIsKillCountUpdated && bIsDeathCountUpdated))
-		{
-			//EPGameState->SetIsScoreBoardReplicated(false);
-			//EPPlayerState->SetIsKillCountReplicated(false);
-			//EPPlayerState->SetIsDeathCountReplicated(false);
-
-			ScoreBoardWidget->UpdateScoreBoard();
-			GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("ClientRPCUpdateScoreBoard_Implementation"));
-		}
+		ScoreBoardWidget->UpdateScoreBoard();
 	}
 }
 
@@ -202,25 +176,26 @@ void AEPPlayerController::ClientRPCSetupEndMatch_Implementation()
 	OnSetupEndMatch.Broadcast();
 }
 
-void AEPPlayerController::ClientRPC_StartMainGame_Implementation()
+void AEPPlayerController::ClientRPCStartMainGame_Implementation()
 {
 	OnStartMainGame.Broadcast();
 }
 
 void AEPPlayerController::SetupEndMatch()
 {
-
 	if (IsLocalController())
 	{
+		// 매치 종료 상태로 위젯 업데이트
 		MatchEndWidget->SetVisibility(ESlateVisibility::Visible);
 		ScoreBoardWidget->TurnOnGrayBackBoard();
 		ShowScoreBoard();
+
+		// 입력 비활성화
+		SetInputMode(FInputModeUIOnly());
+		bShowMouseCursor = true;
+		SetIgnoreMoveInput(true);
+		GetPawn()->DisableInput(this);
 	}
-	SetInputMode(FInputModeUIOnly());
-	bShowMouseCursor = true;
-	//DisableInput(this);
-	SetIgnoreMoveInput(true);
-	GetPawn()->DisableInput(this);
 }
 
 void AEPPlayerController::ClientRPCEndMatch_Implementation()
@@ -274,24 +249,39 @@ void AEPPlayerController::ReceivedPlayer()
 	Super::ReceivedPlayer();
 	if (IsLocalController())
 	{
+		// 클라이언트는 서버에게 서버 시간을 요청한다. 이 때 클라이언트의 요청 시간 또한 함께 전달한다.
 		ServerRequestServerTime(this, GetWorld()->GetTimeSeconds());
 	}
 }
 
-void AEPPlayerController::ClientReportServerTime_Implementation(float requestWorldTime, float serverTime)
+void AEPPlayerController::ServerRequestServerTime_Implementation(APlayerController* Requestor, float RequestWorldTime)
 {
-	float roundTripTime = GetWorld()->GetTimeSeconds() - requestWorldTime;
-	float adjustedTime = serverTime + (roundTripTime * 0.5f);
-	ServerTime = adjustedTime;
+	// 서버는 서버 시간을 구해 클라이언트에게 전달한다. 클라이언트의 요청 시간 또한 함께 전달한다.
+	float WorldServerTime = GetWorld()->GetGameState()->GetServerWorldTimeSeconds();
+	ClientReportServerTime(RequestWorldTime, WorldServerTime);
 }
 
-void AEPPlayerController::ServerRequestServerTime_Implementation(APlayerController* requestor, float requestWorldTime)
+void AEPPlayerController::ClientReportServerTime_Implementation(float RequestWorldTime, float RequestServerTime)
 {
-	float serverTime = GetWorld()->GetGameState()->GetServerWorldTimeSeconds();
-	ClientReportServerTime(requestWorldTime, serverTime);
+	/* 
+	* 클라이언트는 본인이 요청한 시간과 서버 시간을 전달받는다.
+	* 현재 시간에서 클라이언트의 요청 시간을 뺀 값이 클라이언트-서버-클라이언트로 한 번 왕복하는데 소요된 시간이다.
+	*/
+	float RoundTripTime = GetWorld()->GetTimeSeconds() - RequestWorldTime;
+
+	/*
+	* 왕복 시간의 절반은 단방향 전송에 사용되는 시간으로 간주할 수 있다(완벽하게 같지 않을 수 있다).
+	* 전달받은 서버 시간은 서버에서 클라이언트로 전송될 때의 시간이 고려되지 않은 값이다. 
+	* 따라서 서버 시간에 단방향 전송 시간을 더해야만 클라이언트와 서버에서 동일한 시간을 얻을 수 있다.
+	*/
+	float AdjustedTime = RequestServerTime + (RoundTripTime * 0.5f);
+
+	/* 클라이언트는 최종적으로 계산한 시간을 '서버시간'으로 사용한다 */
+	ServerTime = AdjustedTime;
 }
 
-bool AEPPlayerController::ServerRequestServerTime_Validate(APlayerController* requestor, float requestWorldTime)
+
+bool AEPPlayerController::ServerRequestServerTime_Validate(APlayerController* Requestor, float RequestWorldTime)
 {
 	return true;
 }
--- Source/Explosion/Player/EPPlayerController.h
+++ Source/Explosion/Player/EPPlayerController.h
@@ -6,8 +6,21 @@
 #include "GameFramework/PlayerController.h"
 #include "EPPlayerController.generated.h"
 
+class UInputMappingContext;
+class UInputAction;
+class UUserWidget;
+class UEPGameEndWidget;
+class UEPScoreBoardWidget;
+class UEPGameMenuWidget;
+class UEPBasicTextWidget;
+
 DECLARE_MULTICAST_DELEGATE(FOnSetupEndMatch)
 DECLARE_MULTICAST_DELEGATE(FOnStartMainGame)
+
+/**
+ * 본 게임에서 사용되는 플레이어 컨트롤러. 
+ * 게임 진행에 따른 UI와 입력 관리를 담당하며, 이를 위해 서버와 시간 동기화를 진행한다.
+ */
 
 UCLASS()
 class EXPLOSION_API AEPPlayerController : public APlayerController
@@ -21,29 +34,75 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+	/* 플레이어 컨트롤러가 네트워크에 연결되었을 때 호출. 서버에게 시간을 요청한다. */
+	virtual void ReceivedPlayer() override;
+
 protected:
+	/* 게임에 참가한 모든 플레이어가 준비되었을 때, 입력과 UI를 활성화한다 */
 	UFUNCTION()
 	void StartMainGame();
 
-// UI 표시
+// 매치 시작 및 종료
 public:
+	/* 매치가 종료되었을 때 실행할 동작들을 등록하는 대리자*/
 	FOnSetupEndMatch OnSetupEndMatch;
+
+	/* 매치가 시작되었을 때 실행할 동작들을 등록하는 대리자*/
 	FOnStartMainGame OnStartMainGame;
 
+	/* 매치가 시작되었을 때, 클라이언트 측에서 등록된 대리자를 발동 */
 	UFUNCTION(Client, Reliable)
-	void ClientRPC_StartMainGame();
+	void ClientRPCStartMainGame();
 
+	/* 매치가 종료되었을 때, 클라이언트 측에서 등록된 대리자를 발동 */
 	UFUNCTION(Client, Reliable)
 	void ClientRPCSetupEndMatch();
 
-	void SetupEndMatch();
-
+	/* 매치 종료 후 일정 시간이 지났을 때, 서버가 클라이언트의 '로비로 이동'을 강제 발동 */
 	UFUNCTION(Client, Reliable)
 	void ClientRPCEndMatch();
 
-	//UFUNCTION(Client, Reliable)
-	//void ClientRPCUpdateScoreBoard();
+	/* 매치가 종료되었을 때, 클라이언트 측에서 실행할 동작 */
+	void SetupEndMatch();
+	
 
+// 서버 시간 동기화
+public:
+	/* 서버-클라이언트 시간 동기화를 통해 얻은 서버 시간을 반환한다 */
+	float GetServerTime() const { return ServerTime; }
+
+protected:
+	/* 클라이언트에게 현재 서버의 시간과 클라이언트가 요청한 시간을 반환한다 */
+	UFUNCTION(Server, Reliable, WithValidation)
+	void ServerRequestServerTime(APlayerController* Requestor, float RequestWorldTime);
+
+	/* 서버로부터 클라이언트의 요청 시간과 서버 시간을 받는다. 이를 기반으로 동기화된 서버 시간을 계산한다 */
+	UFUNCTION(Client, Reliable)
+	void ClientReportServerTime(float RequestWorldTime, float RequestServerTime);
+
+
+protected:
+	/* 서버-클라이언트간 동기화가 적용된 서버의 시간이다 */
+	float ServerTime;
+
+// 입력 
+public:
+	virtual void SetupInputComponent() override;
+
+protected:
+	UPROPERTY(EditAnyWhere, Category = "Enhanced Input")
+	TObjectPtr<UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(EditAnyWhere, Category = "Enhanced Input")
+	TObjectPtr<UInputAction> ShowingScoreBoard;
+
+	UPROPERTY(EditAnyWhere, Category = "Enhanced Input")
+	TObjectPtr<UInputAction> ShowingGameMenu;
+
+
+// UI 관리
+public:
+	/* 플레이어 컨트롤러가 소유한 스코어보드 위젯의 정보 업데이트 */
 	void UpdateScoreBoard();
 
 	void HideGameMenu();
@@ -52,66 +111,41 @@ protected:
 	void ShowScoreBoard();
 	void HideScoreBoard();
 	void ShowGameMenu();
-	
 
-// 서버 시간 동기화
-public:
-	float GetServerTime() const { return ServerTime; }
-
-protected:
-	virtual void ReceivedPlayer() override;
-
-	UFUNCTION(Client, Reliable)
-	void ClientReportServerTime(float requestWorldTime, float serverTime);
-
-	UFUNCTION(Server, Reliable, WithValidation)
-	void ServerRequestServerTime(APlayerController* requestor, float requestWorldTime);
-
-// 입력 
-public:
-	virtual void SetupInputComponent() override;
-
-protected:
-	UPROPERTY(EditAnyWhere, Category = "Enhanced Input")
-	TObjectPtr<class UInputMappingContext> InputMappingContext;
-
-	UPROPERTY(EditAnyWhere, Category = "Enhanced Input")
-	TObjectPtr<class UInputAction> ShowingScoreBoard;
-
-	UPROPERTY(EditAnyWhere, Category = "Enhanced Input")
-	TObjectPtr<class UInputAction> ShowingGameMenu;
-
-
-// UI 
 protected:
 	UPROPERTY(EditAnyWhere, Category = "UI")
-	TSubclassOf<class UUserWidget> MatchEndWidgetClass;
+	TSubclassOf<UUserWidget> MatchEndWidgetClass;
 
 	UPROPERTY(EditAnyWhere, Category = "UI")
-	TSubclassOf<class UEPScoreBoardWidget> ScoreBoardWidgetClass;
+	TSubclassOf<UEPScoreBoardWidget> ScoreBoardWidgetClass;
 
 	UPROPERTY(EditAnywhere, Category = "UI")
-	TSubclassOf<class UEPGameMenuWidget> GameMenuWidgetClass;
+	TSubclassOf<UEPGameMenuWidget> GameMenuWidgetClass;
 
 	UPROPERTY(EditAnywhere, Category = "UI")
-	TSubclassOf<class UEPBasicTextWidget> StartMainGameWidgetClass;
+	TSubclassOf<UEPBasicTextWidget> StartMainGameWidgetClass;
 
 	UPROPERTY()
-	TObjectPtr<class UEPGameEndWidget> MatchEndWidget;
+	TObjectPtr<UEPGameEndWidget> MatchEndWidget;
 
 	UPROPERTY()
-	TObjectPtr<class UEPScoreBoardWidget> ScoreBoardWidget;
+	TObjectPtr<UEPScoreBoardWidget> ScoreBoardWidget;
 
 	UPROPERTY()
-	TObjectPtr<class UEPGameMenuWidget> GameMenuWidget;
+	TObjectPtr<UEPGameMenuWidget> GameMenuWidget;
 
 	UPROPERTY()
-	TObjectPtr<class UEPBasicTextWidget> StartMainGameWidget;
-
-protected:
-	float ServerTime;
+	TObjectPtr<UEPBasicTextWidget> StartMainGameWidget;
 
 
+// 게임 시작
 private:
+	/*
+	* 레벨 이동 직후에는 UI에 표시할 플레이어 정보가 충분히 준비되지 않는다.
+	* 따라서 레벨 이동 직후에는 UI를 숨기고, 매치가 시작되었을 때 UI를 표시하기 위한 플래그이다.
+	*/
 	uint8 bIsStartMainGame : 1;
+
+	/* 매치가 시작되었을 때, 이를 알리는 UI가 유지되는 기간 */
+	float StartMainGameWidgetHideDelay;
 };
